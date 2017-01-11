757304208aff20d4539516551194c4485e6eb806
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2017-01-11 10:34:51 -0400

    Added Source for Compare Function

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 #include <time.h>
 #include <MagickCore/MagickCore.h>
 
+/*
+  https://www.tutorialspoint.com/c_standard_library/c_function_qsort.htm
+*/
 int compare( const void* a, const void* b)
 {
    return ( *(int*)b - *(int*)a );
