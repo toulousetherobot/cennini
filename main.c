ffb94d8ddaca6013642d3fe9a2ba92ec8c189444
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2017-01-21 15:51:34 -0500

    Added SobelOperator

--- main.c
+++ main.c
@@ -9,6 +9,35 @@
 #define MagickMax(x,y)  (((x) > (y)) ? (x) : (y))
 #define MagickMin(x,y)  (((x) < (y)) ? (x) : (y))
 
+enum SobelFilter {
+  SobelXFilter,
+  SobelYFilter
+};
+
+
+MagickExport Image *SobelOperator(const Image *image, enum SobelFilter filter, ExceptionInfo *exception)
+{
+  KernelInfo *kernel;
+  Image *sobel_image;
+
+  assert(image != (const Image *) NULL);
+  assert(image->signature == MagickCoreSignature);
+
+  assert(exception != (ExceptionInfo *) NULL);
+  assert(exception->signature == MagickCoreSignature);
+
+  // https://en.wikipedia.org/wiki/Sobel_operator
+  if (filter == SobelXFilter)
+    kernel = AcquireKernelInfo("3: -1,0,1  -2,0,2  -1,0,1", exception);
+  else if (filter == SobelYFilter)
+    kernel = AcquireKernelInfo("3: 1,2,1  0,0,0  -1,-2,-1", exception);
+
+  sobel_image = MorphologyImage(image, ConvolveMorphology, 1, kernel, exception);
+
+  kernel = DestroyKernelInfo(kernel);
+
+  return(sobel_image);
+}
 
 static inline MagickBooleanType SetImageProgress(const Image *image, const char *tag,const MagickOffsetType offset,const MagickSizeType extent)
 {
